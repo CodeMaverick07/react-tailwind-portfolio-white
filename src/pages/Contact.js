e4494ed4bbfba6fafdea8469b00dba4903f0e56b
Hemant Jatal <111072612+TomatoTwister@users.noreply.github.com>
2023-06-25 20:02:57 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import {
   FaInstagram,
-  FaLinkedin,
+  FaWhatsapp,
   FaMailBulk,
   FaGithub,
   FaPhoneAlt,
@@ -14,7 +14,7 @@ const Contact = () => {
       <div className="overflow-hidden">
         <div className="h-screen  ">
           <div className="h-screen flex  items-center flex-col text-5xl md:text-3xl ">
-            <div className="fixed z-20 flex mt-28 border-2 px-1 py-2 rounded-md border-black hover:bg-gray-600 hover:opacity-80 shadow-2xl md:px-0 md:py-1 opacity-80">
+            <div className="fixed z-20 flex mt-28 border-2 px-1 py-2 rounded-md border-black hover:bg-gray-200 hover:opacity-80 shadow-2xl md:px-0 md:py-1 opacity-80">
               <a href="tel:+919130921658">
                 <FaPhoneAlt className="mx-10 p-1 border-2 border-black rounded-md hover:bg-black hover:text-white md:mx-5 " />
               </a>
@@ -40,7 +40,10 @@ const Contact = () => {
                 <FaMailBulk className="mx-10 p-1 border-2 border-black rounded-md hover:bg-black hover:text-white md:mx-5" />
               </a>
 
-              <FaLinkedin className="mx-10 p-1 border-2 border-black rounded-md hover:bg-black hover:text-white md:mx-5" />
+             <a  target="_blank"
+                rel="noreferrer"
+                  href="https://wa.me/+919130921658"
+                  > <FaWhatsapp className="mx-10 p-1 border-2 border-black rounded-md hover:bg-black hover:text-white md:mx-5" /></a>
             </div>
             <div className="mt-20 h-screen ">
               <lottie-player
